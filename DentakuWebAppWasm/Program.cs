b46e945d351e995550529f0b6a1c0297f1bf2f29
logtaka <logtac00@gmail.com>
2025-11-24 19:28:58 +0900

    Add Blazor WASM console interface

--- DentakuWebAppWasm/Program.cs
+++ DentakuWebAppWasm/Program.cs
@@ -9,16 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// ★ Consoleアプリの依存関係注入
 builder.Services.AddSingleton<ConsoleHost>();
 
-var host = builder.Build();
-
-// ★ WASM用 Console の初期化 → C# Main() をバックグラウンド実行
-_ = Task.Run(async () =>
-{
-    var console = host.Services.GetRequiredService<ConsoleHost>();
-    await console.StartAsync();
-});
-
-await host.RunAsync();
+await builder.Build().RunAsync();
