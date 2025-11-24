1fd09ddad6e889cb53918c15bfe38f584a71ceef
logtaka <logtac00@gmail.com>
2025-11-24 19:13:45 +0900

    web プログラムを利用

--- DentakuWebAppWasm/Program.cs
+++ DentakuWebAppWasm/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using DentakuWebAppWasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,4 +9,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+// ★ Consoleアプリの依存関係注入
+builder.Services.AddSingleton<ConsoleHost>();
+
+var host = builder.Build();
+
+// ★ WASM用 Console の初期化 → C# Main() をバックグラウンド実行
+_ = Task.Run(async () =>
+{
+    var console = host.Services.GetRequiredService<ConsoleHost>();
+    await console.StartAsync();
+});
+
+await host.RunAsync();
